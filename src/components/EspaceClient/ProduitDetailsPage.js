1afa4f33af6d51da6747436101b46f9f94c52cc1
Derbali Karim <derbalikarim182@gmail.com>
2024-06-03 16:02:41 -1100

    contract modal done

--- src/components/EspaceClient/ProduitDetailsPage.js
+++ src/components/EspaceClient/ProduitDetailsPage.js
@@ -1,50 +1,59 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import MaterialTable from 'material-table';
+import tableIcons from '../MaterialTableIcons';
+
 
 export default function ProduitDetails() {
     const location = useLocation();
     const produits = location.state.produit;
-    console.log(produits);
-
-    const columns = [
-      {
-          title: <h6 style={{ fontSize: '17px', color: '#f48404' }}>Nom Commercial</h6>,
-          field: 'ref_produit_contrat'
-      },
-        {
-            title: <h6 style={{ fontSize: '17px', color: '#f48404' }}>Nom Commercial</h6>,
-            field: 'nom_commercial'
-        },
-        {
-            title: <h6 style={{ fontSize: '17px', color: '#f48404' }}>Référence</h6>,
-            field: 'reference'
-        },
-        {
-            title: <h6 style={{ fontSize: '17px', color: '#f48404' }}>Etat</h6>,
-            field: 'etat'
-        },
-        {
-            title: <h6 style={{ fontSize: '17px', color: '#f48404' }}>Etat Service</h6>,
-            field: 'etat_service'
-        }
-    ];
 
     return (
-        <MaterialTable
-            columns={columns}
-            data={produits}
-            title={<h4>Liste des Produits</h4>}
-            options={{
-                search: false,
-                paging: false,
-                filtering: false,
-                exportButton: false,
-                headerStyle: {
-                    backgroundColor: '#01579b',
-                    color: '#FFF'
-                }
-            }}
-        />
+        <div>
+            <MaterialTable
+                columns={[
+                    {
+                        title: 'Nom Commercial',
+                        field: 'nom_commercial'
+                    },
+                    {
+                        title: 'Référence',
+                        field: 'reference'
+                    },
+                    {
+                        title: 'Etat',
+                        field: 'etat'
+                    },
+                    {
+                        title: 'Etat Service',
+                        field: 'etat_service'
+                    },
+                    {
+                        title: 'Action',
+                        render: rowData => (
+                            <div>
+                                <button className='btn' style={{ borderRadius: 19, borderColor: '#18a6f0', backgroundColor: '#18a6f0', color: "#fff" }} onClick="">
+                                    <i className="fas fa-credit-card" style={{ marginRight: '8px' }}></i>
+                                    Acheter
+                                </button>
+                            </div>
+                        )
+                    }
+                ]}
+                data={produits}
+                title={<h4>Liste des Produits</h4>}
+                icons={tableIcons}
+                options={{
+                    search: true,
+                    paging: true,
+                    pageSize: 5,
+                    pageSizeOptions: [5, 10, 20],
+                    headerStyle: {
+                        backgroundColor: '#01579b',
+                        color: '#FFF'
+                    }
+                }}
+            />
+        </div>
     );
 }
